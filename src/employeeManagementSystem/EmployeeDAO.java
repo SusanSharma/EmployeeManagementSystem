e270eb42907ac2a00a52291062cb9b52585af6c2
Susan Sharma <Susansharma215@gmail.com>
2024-06-17 14:43:37 +0545

    minor changes

--- src/employeeManagementSystem/EmployeeDAO.java
+++ src/employeeManagementSystem/EmployeeDAO.java
@@ -92,9 +92,9 @@ public class EmployeeDAO {
             	employees.add(emp);
             	
             	//In case you wanted to print one by one
-                System.out.println(rs.getInt(1) + " | " + rs.getString(2) +
-                                   " " + rs.getString(3) + " | " + rs.getString(4) +
-                                   " | " + rs.getString(5) + " | " + rs.getString(6));
+//                System.out.println(rs.getInt(1) + " | " + rs.getString(2) +
+//                                   " " + rs.getString(3) + " | " + rs.getString(4) +
+//                                   " | " + rs.getString(5) + " | " + rs.getString(6));
             }
         } catch (SQLException e) {
             e.printStackTrace();
